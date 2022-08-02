ec7bd745844fc8861ae28cbdfbad1d7813e69469
ыÑsonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-08-02 22:45:03 +0400

    uncooment migrations

--- migrations/1656783178135_marks.js
+++ migrations/1656783178135_marks.js
@@ -3,33 +3,33 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-   // pgm.createTable("marks", {
-   //    id: {
-   //       type: "id",
-   //       autoIncrement: true
-   //    },
-   //    user_id: {
-   //       type: "integer",
-   //       references: '"users"',
-   //       onDelete: "cascade",
-   //       notNull: true
-   //    },
-   //    content: {
-   //       type: "varchar(255)",
-   //       notNull: true
-   //    },
-   //    is_important: {
-   //       type: "boolean",
-   //       notNull: true
-   //    },
-   //    created_at: {
-   //       type: "timestamp",
-   //       notNull: true
-   //    }
-   // });
-   // pgm.sql(`ALTER TABLE "marks" ADD CONSTRAINT "user_id_content_unique" UNIQUE(content,user_id)`);
+   pgm.createTable("marks", {
+      id: {
+         type: "id",
+         autoIncrement: true
+      },
+      user_id: {
+         type: "integer",
+         references: '"users"',
+         onDelete: "cascade",
+         notNull: true
+      },
+      content: {
+         type: "varchar(255)",
+         notNull: true
+      },
+      is_important: {
+         type: "boolean",
+         notNull: true
+      },
+      created_at: {
+         type: "timestamp",
+         notNull: true
+      }
+   });
+   pgm.sql(`ALTER TABLE "marks" ADD CONSTRAINT "user_id_content_unique" UNIQUE(content,user_id)`);
 };
 
 exports.down = (pgm) => {
-   // pgm.dropTable("marks");
+   pgm.dropTable("marks");
 };
