403d8135727cea70b4c76be580bd7511735b0853
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-08-13 00:00:41 +0400

    add: temporary cors disable

--- src/server/packages/cors/cors.ts
+++ src/server/packages/cors/cors.ts
@@ -3,15 +3,15 @@ import { GetAppConfig } from "../config/config";
 const err = new Error("CORS ERROR");
 
 export function HandleCors(origin: string, cb) {
-   const originsProd = ["localhost:80", "localhost:443", "http://localhost:80", "https://localhost:443"];
-
+   const originsProd = ["localhost:80", "localhost:443", "http://localhost:80", "https://localhost:443", "sanchofood.ru", "https://sanchofood.ru"];
    const config = GetAppConfig();
 
    if (config.env.nodeEnv === "production") {
-      if (originsProd.includes(origin)) {
-         return cb(null, origin);
-      }
-      return cb(err, null);
+      //todo: cors
+      // if (originsProd.includes(origin)) {
+      //    return cb(null, origin);
+      // }
+      return cb(null, origin);
    }
 
    return cb(null, origin);
