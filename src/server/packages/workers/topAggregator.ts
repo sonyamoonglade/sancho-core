6217444b41afe03e1745d611a4ee915a4bd713b1
sonyamoonglade <aalexandrovich.hireme@gmail.com>
2022-08-13 16:46:18 +0400

    fix: aggregator algo

--- src/server/packages/workers/topAggregator.ts
+++ src/server/packages/workers/topAggregator.ts
@@ -6,7 +6,6 @@ const top: ProductTop = new Map<string, number>();
 const topArr: ProductTopArray = [];
 //Calculate how many product total ( to later count %'s)
 let totalProductAmount: number = 0;
-
 for (const cart of carts) {
    for (const product of cart) {
       totalProductAmount += product.quantity;
@@ -22,11 +21,13 @@ for (const cart of carts) {
       top.set(product.translate, v + product.quantity);
    }
 }
+
 //Iterate over ["mozarella",25]... in the map and Sort descending
 for (const [tr, q] of Array.from(top.entries()).sort(([_, q1], [__, q2]) => q2 - q1)) {
+   const percent = Math.ceil((q / (totalProductAmount === 0 ? 1 : totalProductAmount)) * 100);
    topArr.push({
       translate: tr,
-      percent: Math.ceil(q / (totalProductAmount === 0 ? totalProductAmount : 1)), // Make sure to not divide by zero
+      percent, // Make sure to not divide by zero
       exactq: q
    });
 }
